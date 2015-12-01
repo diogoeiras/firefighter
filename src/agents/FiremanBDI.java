4fae6d6c789b2b6886725bb97b5bb95d86bee5e1
Eiras <eiras@outlook.com>
2015-12-01 16:00:30 +0000

    Recursive goal working.

--- src/agents/FiremanBDI.java
+++ src/agents/FiremanBDI.java
@@ -1,10 +1,7 @@
 package agents;
 
 import jadex.bdiv3.BDIAgent;
-import jadex.bdiv3.annotation.Belief;
-import jadex.bdiv3.annotation.Plan;
-import jadex.bdiv3.annotation.PlanPassed;
-import jadex.bdiv3.annotation.Trigger;
+import jadex.bdiv3.annotation.*;
 import jadex.bdiv3.runtime.impl.PlanFailureException;
 import jadex.extension.envsupport.environment.ISpaceObject;
 import jadex.extension.envsupport.environment.space2d.Grid2D;
@@ -22,6 +19,10 @@ public class FiremanBDI {
 
     @Belief
     protected Grid2D space = (Grid2D)fireman.getParentAccess().getExtension("2dspace").get();
+
+
+    @Belief(updaterate = 500)
+    protected long currentTime = System.currentTimeMillis();
 
     @Belief
     protected ISpaceObject myself = space.getAvatar(fireman.getComponentDescription(), fireman.getModel().getFullName());
@@ -44,23 +45,49 @@ public class FiremanBDI {
 
 
         // TODO: Eliminate this DEBUG
-        fireman.waitForDelay(1500).get();
 
         FiremanGoal goal = (FiremanGoal) fireman.dispatchTopLevelGoal(Goal).get();
 
     }
 
     @Plan(trigger = @Trigger(goals=FiremanGoal.class))
-    protected  void changePosition(FiremanGoal goal){
-        Vector2Int currentPosition = goal.getCurrentPosition();
+    public class MovingPlan{
+        @PlanBody
+        protected  void changePosition(FiremanGoal goal){
+            Vector2Int currentPosition = goal.getCurrentPosition();
 
-        if (currentPosition.getXAsInteger() != goal.getDesiredPosition().getXAsInteger() ||
-                currentPosition.getYAsInteger() != goal.getDesiredPosition().getYAsInteger()){
+            if (currentPosition.getXAsInteger() != goal.getDesiredPosition().getXAsInteger() ||
+                    currentPosition.getYAsInteger() != goal.getDesiredPosition().getYAsInteger()){
 
-            goal.setCurrentPosition(new Vector2Int(currentPosition.getXAsInteger() - 1 ,currentPosition.getYAsInteger() - 1));
-            myself.setProperty("position", new Vector2Int(goal.getCurrentPosition().getXAsInteger(),goal.getCurrentPosition().getYAsInteger()));
+                int difX;
+                int difY;
 
+                if(currentPosition.getXAsInteger()==goal.getDesiredPosition().getXAsInteger()){
+                    difX = 0;
+                }
+                else{
+                    difX = (goal.getDesiredPosition().getXAsInteger()-currentPosition.getXAsInteger());
+                    difX = difX/Math.abs(difX);
+                }
+                if(currentPosition.getYAsInteger()==goal.getDesiredPosition().getYAsInteger()){
+                    difY = 0;
+                }
+                else{
+                    difY = (goal.getDesiredPosition().getYAsInteger()-currentPosition.getYAsInteger());
+                    difY = difY/Math.abs(difY);
+                }
+                goal.setCurrentPosition(new Vector2Int(currentPosition.getXAsInteger() + difX ,currentPosition.getYAsInteger() + difY));
+                myself.setProperty("position", new Vector2Int(goal.getCurrentPosition().getXAsInteger(),goal.getCurrentPosition().getYAsInteger()));
+                throw new PlanFailureException();
+            }
         }
+
+        @PlanPassed
+        public void passed() {
+            System.out.println("Reached destination");
+        }
+
     }
+
 
 }
--- src/goals/FiremanGoal.java
+++ src/goals/FiremanGoal.java
@@ -1,9 +1,11 @@
 package goals;
 
 import jadex.bdiv3.annotation.Goal;
+import jadex.bdiv3.annotation.GoalRecurCondition;
+import jadex.bdiv3.runtime.ChangeEvent;
 import jadex.extension.envsupport.math.Vector2Int;
 
-@Goal
+@Goal(recur=true)
 public class FiremanGoal {
 
     protected Vector2Int desiredPosition;
@@ -28,5 +30,13 @@ public class FiremanGoal {
 
     public void setDesiredPosition(Vector2Int desiredPosition){
         this.desiredPosition = desiredPosition;
+    }
+
+    @GoalRecurCondition(beliefs="currentTime")
+    public boolean checkRecur(ChangeEvent event) {
+        if(currentPosition != desiredPosition)
+            return true;
+        else
+            return false;
     }
 }
